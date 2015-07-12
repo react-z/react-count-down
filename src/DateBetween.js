20f31cf72deb23e1fdae70f0838f760be3d06115
Chad Paulson <chad.paulson@gmail.com>
2015-07-11 22:22:46 -0500

    Checks for singular value and displays day, hour, minute and second accordingly.

--- src/DateBetween.js
+++ src/DateBetween.js
@@ -14,10 +14,39 @@ let DateBetween = function(startDate, endDate) {
   let minutes = Math.floor((distance % hour) / minute);
   let seconds = Math.floor((distance % minute) / second);
 
-  let between = days + ' days ';
-  between += hours + ' hours ';
-  between += minutes + ' minutes ';
-  between += seconds + ' seconds';
+  var day_description;
+  var hour_description;
+  var min_description;
+  var sec_description;
+
+  if(days == 1) {
+    day_description = ' day ';
+  } else {
+    day_description = ' days ';
+  }
+
+  if(hours == 1) {
+    hour_description = ' hour ';
+  } else {
+    hour_description = ' hours ';
+  }
+
+  if(minutes == 1) {
+    min_description = ' minute ';
+  } else {
+    min_description = ' minutes ';
+  }
+
+  if(seconds == 1) {
+    sec_description = ' second';
+  } else {
+    sec_description = ' seconds';
+  }
+
+  let between = days + day_description;
+  between += hours + hour_description;
+  between += minutes + min_description;
+  between += seconds + sec_description;
 
   return between;
 }
