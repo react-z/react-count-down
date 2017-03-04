d11c7fc77f6adb81a9e46613317109f13ff08080
Steven Martin <stevenisekimartin@gmail.com>
2017-03-05 01:07:35 +1100

    Add yarn

--- test/DateBetween.spec.js
+++ test/DateBetween.spec.js
@@ -3,9 +3,9 @@ var DateBetween = require('../lib/DateBetween.js');
 
 var date1 = new Date("Fri Feb 06 2015 18:07:30");
 var date2 = new Date("Fri Feb 07 2015 18:07:30");
-var difference = DateBetween(date1,date2);
+var difference = DateBetween(date1, date2);
 
 test('difference between 2 dates is equal to 1 day', function (t) {
-    t.equal(difference, "1 days 0 hours 0 minutes 0 seconds");
+    t.equal(difference, "1 day");
     t.end();
 });
